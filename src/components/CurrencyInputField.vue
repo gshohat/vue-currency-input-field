9af96d4f5508efd12a5a1859feed91aed9719b29
gshohat <gilad.shohat@gmail.com>
2023-09-09 12:16:54 +0200

    onChange@add $ char

--- src/components/CurrencyInputField.vue
+++ src/components/CurrencyInputField.vue
@@ -1,8 +1,24 @@
 <script setup>
+const props = defineProps(['options']);
+
+function handleOnChange(event) {
+  const value = event.target.value;
+  const dollarChar = /\$/;
+  if (!value.match(dollarChar))
+    event.target.value = `$${event.target.value}`;
+
+}
 
 </script>
 
+
 <template>
+  <input
+      type="text"
+      name="money"
+      placeholder="Please enter a $ amount"
+      @input="handleOnChange"
+  />
 
 </template>
 
