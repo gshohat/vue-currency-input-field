bd8b1e9ae9f4dd0e240e7c23483714b14a68b9d0
gshohat <gilad.shohat@gmail.com>
2023-09-11 08:38:05 +0200

    readme@modify

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Vue Currency Input Field
 
-Stealth mode 🤫
+![NPM License](https://img.shields.io/npm/l/vue-currency-input-field)
+
+**Lightweight** currency input component for [Vue](https://vuejs.org/) **< 5k** 😎 <br>
+Emits the formatted value to parent component
+
+## Usage
+
+`npm i vue-currency-input-field`
+
+```
+<script setup>
+import CurrencyInputField from 'vue-currency-input-field';
+
+const options = {
+  currency: '$',
+  placeHolder: 'Please enter a number',
+};
+
+function handleValueChange(value) {
+  //todo value '$123,456'
+}
+</script>
+
+
+<template>
+<CurrencyInputField :options="options" @value-change="handleValueChange"/>
+</template
+```
+
 
 ## Contact
 Feel free to ping me 💫
--- src/components/CurrencyInputField.vue
+++ src/components/CurrencyInputField.vue
@@ -1,22 +1,42 @@
 <script setup>
 const props = defineProps(['options']);
+const emit = defineEmits(['valueChange']);
 
 function handleOnChange(event) {
   const value = event.target.value;
-  const dollarChar = /\$/;
-  if (!value.match(dollarChar))
-    event.target.value = `$${event.target.value}`;
 
+  const regex = new RegExp(`[${props.options.currency},]`);
+  const valueWithoutSpecialChars = value.split(regex).join('');
+  const num = parseInt(valueWithoutSpecialChars);
+
+  let currencyRegexStr;
+  props.options.currency === '$' ?
+      currencyRegexStr = `\\${props.options.currency}` :
+      currencyRegexStr = props.options.currency;
+  const currencyRegex = new RegExp(currencyRegexStr);
+  if (!value.match(currencyRegex))
+    event.target.value = `${props.options.currency}${event.target.value}`;
+  else if (num > 999) {
+    event.target.value = `${props.options.currency}${addComma(valueWithoutSpecialChars)}`;
+  }
+  emit('valueChange', event.target.value);
+}
+
+function addComma(str) {
+  if (str.length < 4)
+    return str;
+  const prefixComma = addComma(str.slice(0, str.length - 3));
+  const suffixComma = str.slice(str.length - 3, str.length);
+  return `${prefixComma},${suffixComma}`;
 }
 
 </script>
-
 
 <template>
   <input
       type="text"
       name="money"
-      placeholder="Please enter a $ amount"
+      :placeholder="options.placeHolder"
       @input="handleOnChange"
   />
 
